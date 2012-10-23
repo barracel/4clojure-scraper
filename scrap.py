93c886898774f37d5343420fccb25c0d8fa3195c
barracel <fernandez.barracel@gmail.com>
2012-10-23 23:45:06 +0200

    Refactor template, added cache decorator

--- scrap.py
+++ scrap.py
@@ -1,16 +1,22 @@
+import collections
+import functools
 import itertools
+import os
 import pickle
 import re
 import sys
+import textwrap
 
 from BeautifulSoup import BeautifulSoup
 from gevent import local
 from gevent import monkey
 from gevent.pool import Pool
 import requests
-import os
 
 monkey.patch_all()
+
+Problem = collections.namedtuple('Problem', ('id', 'title', 'text'))
+Solution = collections.namedtuple('Solution', ('id', 'text'))
 
 class Scraper(object):
     local = local.local()
@@ -34,9 +40,10 @@ class Scraper(object):
                            num_problem,
                            verify=False)
         if "You must solve this problem" in res.text:
-            return (num_problem, None)
-        soup = BeautifulSoup(res.text)
-        return (num_problem, soup.find('pre').text)
+            return Solution(num_problem, None)
+        soup = BeautifulSoup(res.text,
+                             convertEntities=BeautifulSoup.HTML_ENTITIES)
+        return Solution(num_problem, soup.find('pre').text)
 
     def scrap_problem(self, num_problem):
         print 'p', num_problem
@@ -44,39 +51,72 @@ class Scraper(object):
         res = rsession.get('https://www.4clojure.com/problem/%s' %
                            num_problem,
                            verify=False)
-        fulltext = str(BeautifulSoup(res.text).find('div',
-                                                    {'id': 'prob-desc'}))
-        text = re.match('<div id="prob-desc">(.*)<br', fulltext).group(1)
-        return (num_problem, text)
+        soup = BeautifulSoup(res.text,
+                             convertEntities=BeautifulSoup.HTML_ENTITIES)
+        titlediv = str(soup.find('div', {'id': 'prob-title'}))
+        title = re.match('<div id="prob-title">(.*?)</div>', titlediv).group(1)
+        textdiv = str(soup.find('div', {'id': 'prob-desc'}))
+        text = re.match('<div id="prob-desc">(.*?)<br', textdiv).group(1)
+        return Problem(num_problem, title, text)
 
+def cache(func):
+    @functools.wraps(func)
+    def decorator(*args, **kwargs):
+        call_args = ''.join(str(x) for x in itertools.chain(args,
+                                                            kwargs.values()))
+        cache_file = './.cache_%s' % hash(call_args)
+        if os.path.exists(cache_file):
+            return load(cache_file)
+        ret = func(*args, **kwargs)
+        save(ret, cache_file)
+        return ret
+    return decorator
+
+def save(data, filename):
+    with open(filename, 'w') as f:
+        pickle.dump(data, f)
+
+def load(filename):
+    with open(filename) as f:
+        return pickle.load(f)
+
+@cache
 def do_scrap(num_problems, user, password):
     scraper = Scraper(user, password)
     p = Pool(20)
     problems = list(p.imap_unordered(scraper.scrap_problem,
                                      range(0, num_problems)))
-    solutions = list(p.imap_unordered(scraper.scrap_solution,
-                                      range(0, num_problems)))
-    solutions = [s for s in solutions if s[1]]
-    save(problems, solutions)
+    solutions = list(s for s in p.imap_unordered(scraper.scrap_solution,
+                                                 range(0, num_problems))
+                     if s and s.text)
     return problems, solutions
 
-def save(*args):
-    with open('./data.pickle', 'w') as f:
-        pickle.dump(args, f)
+template_src = """
+; {problem.id}. {problem.title}
+; https://www.4clojure.com/problem/{problem.id}
+; {problem_text}
 
-def load():
-    with open('./data.pickle') as f:
-        return pickle.load(f)
+{solution.text}
 
-def main(user, password):
-    problems, solutions = do_scrap(200, user, password)
-    #problems, solutions = load()
-    problems = dict(problems)
+"""
+def src_lines(problems, solutions):
+    for solution in solutions:
+        problem_text = '\n; '.join(textwrap.wrap(problems[solution.id].text))
+        yield template_src.format(problem=problems[solution.id],
+                                  problem_text=problem_text,
+                                  solution=solution)
+
+def main(user, password, output_file):
+    problems, solutions = do_scrap(290, user, password)
+    problems = dict((p.id, p) for p in problems)
     solutions = sorted(solutions, key=lambda x: x[0])
+    with open(output_file, 'w') as f:
+        f.write('\n'.join(src_lines(problems, solutions)))
     return problems, solutions
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print "Usage: python %s user password" % os.path.basename(__file__)
+    if len(sys.argv) < 4:
+        print ("Usage: python %s user password output_file"
+               % os.path.basename(__file__))
         sys.exit(1)
-    problems, solutions = main(*sys.argv[1:3])
+    problems, solutions = main(*sys.argv[1:4])
